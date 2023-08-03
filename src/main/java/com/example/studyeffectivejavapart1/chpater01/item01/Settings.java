83bb155967480d5165ae466f53d7155e42db8a0e
sanghun <bsh20057@gmail.com>
2023-08-03 23:29:26 +0900

    perf(item01): 생성자 대신 정적팩터리 메서드를 고려하라 - 단점 - 만약 정적 팩토리를 제공하면서 기본 생성자를 private 로 생성하는 경우 해당 클래스의 상속이 막힌다 - javadocs 으로 해당 클래스를 문서화 하는 경우 정적팩토리만 있으면 Constructor Summary 를 제공하지 않아서 객체 생성에 대한 패턴을 알기 힘들다 ( - 따라서 팩토리 메서드에 대한 메서드 명에 대한 규칙을 잘 따르는게 좋다 - 가장 좋은건 주석을 통해서 문서화 시키는 것 )

--- src/main/java/com/example/studyeffectivejavapart1/chpater01/item01/Settings.java
+++ src/main/java/com/example/studyeffectivejavapart1/chpater01/item01/Settings.java
@@ -3,6 +3,9 @@ package com.example.studyeffectivejavapart1.chpater01.item01;
 /**
  * 생성자로는 인스턴스의 제공을 컨트롤 할 수 없지만
  * 정적 팩토리 메서드를 사용해서 하나의 인스턴스만 만들게 처리할 수 있다
+ *
+ * 이 클래스의 인스턴스는 #getInstance() 를 통해 사용합니다
+ * @see #getInstance()
  */
 public class Settings {
 
@@ -12,10 +15,12 @@ public class Settings {
 
     private Difficulty difficulty;
 
+    // 아래와 같이 기본 생성자를 private 로 막으면 해당 클래스의 상속이 불가능해진다
     private Settings() {}
 
     private static final Settings SETTINGS = new Settings();
 
+    // Constructor Summary 에 표시되지 않음
     public static Settings getInstance() {
         return SETTINGS;
     }
