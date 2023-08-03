886d07ab44158312d51aefc96c391cf3b88cf988
sanghun <bsh20057@gmail.com>
2023-08-03 23:20:56 +0900

    perf(item01): 생성자 대신 정적팩터리 메서드를 고려하라 - 3 - 정적팩터리 메서드 타입을 사용하면 인터페이스를 사용해서 구현 클래스를 제공할 수 있다 - 정적팩터리 메서드만 있으면 인터페이스만 존재하고 구현체가 존재하지 않아도 된다 ( - 실제로는 구현체가 존재하지만 호출시 직접 주입하지 않아도 된다는 뜻 - ServiceLoader 를 사용해서 의존성을 제거하고 구현체를 사용할 수 있다 - 어떠한 구현체가 올 지 모르는 경우가 있기 때문에 위와 같은 방식을 사용하면 임의의 구현체를 가지고 구현할 수 있다 )

---
+++ src/main/java/com/example/studyeffectivejavapart1/chpater01/item01/EnglishHelloService.java
@@ -0,0 +1,8 @@
+package com.example.studyeffectivejavapart1.chpater01.item01;
+
+public class EnglishHelloService implements HelloService {
+    @Override
+    public String hello() {
+        return "hello";
+    }
+}
---
+++ src/main/java/com/example/studyeffectivejavapart1/chpater01/item01/HelloServiceFactory.java
@@ -0,0 +1,11 @@
+package com.example.studyeffectivejavapart1.chpater01.item01;
+
+public class HelloServiceFactory {
+
+    public static void main(String[] args) {
+        // 입력되는 파라미터에 따라 다른 구현체를 제공한다
+        String hello = HelloService.of("ko").hello();
+        System.out.println("hello = " + hello);
+    }
+
+}
