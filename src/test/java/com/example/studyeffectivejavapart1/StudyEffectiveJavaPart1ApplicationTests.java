dcbae8d8dfb61757a8f3d6dc7c27fdf854404461
sanghun <bsh20057@gmail.com>
2023-08-03 22:52:46 +0900

    Initial commit

---
+++ src/test/java/com/example/studyeffectivejavapart1/StudyEffectiveJavaPart1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.studyeffectivejavapart1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudyEffectiveJavaPart1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
