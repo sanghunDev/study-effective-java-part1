6128ec0f9afa99daf520bdc5fd720f6b46e8272e
sanghun <bsh20057@gmail.com>
2023-08-04 23:34:08 +0900

    perf(item01): 열거타입 - 상수 목록을 담을 수 있는 데이터 타입이다 - 특정한 변수가 가질 수 있는 값을 제한 가능하며 타입 세이프티를 보장 가능하다 - 싱글톤 패턴을 구현할 때 사용하기도 한다 - Map 이나 Set 을 구현 시 키값이 Enum 타입이라면 EnumMap 이나 EnumSet 을 사용하는게 성능적으로 유리하다

--- src/main/java/com/example/studyeffectivejavapart1/chpater01/item01/Order.java
+++ src/main/java/com/example/studyeffectivejavapart1/chpater01/item01/Order.java
@@ -1,5 +1,8 @@
 package com.example.studyeffectivejavapart1.chpater01.item01;
 
+import java.util.Arrays;
+import java.util.EnumMap;
+
 public class Order {
 
     private boolean prime;
@@ -7,6 +10,9 @@ public class Order {
     private boolean urgent;
 
     private Product product;
+
+    // 해당 필드는 해당 클래스의 정해진 값만 가질 수 있음 (Type-Safety)
+    private OrderStatus orderStatus;
 
     // 정적 팩토리를 사용해서 메소드에 맞게 이름을 부여해서 메소드의 표현을 더 잘 할 수 있다
     // 시그니처 중복 시 유용하다
@@ -26,8 +32,14 @@ public class Order {
     }
 
     public static void main(String[] args) {
-
         Order order = new Order();
+        // enum 타입이 가지고 있는 모든 값을 순회하며 출력
+        Arrays.stream(OrderStatus.values()).forEach(System.out::println);
+
+        // enum 타입의 Map 생성
+        // Hash 는 해싱 작업을 통해 성능 향상을 이루지만 Enum 은 단일 객체를 보장하기 때문에 해싱이 필요가 없어 검색 속도가 빠르다
+        // Enum 에 명시된 순서를 기억한다
+        EnumMap<OrderStatus, Object> enumMap = new EnumMap<>(OrderStatus.class);
 
     }
 
