b7a85b2bbe086dc42d6aafdb2ff9b22ee26d5001
sanghun <bsh20057@gmail.com>
2023-08-10 22:53:22 +0900

    feat(item04): 인스턴스화를 막으려거든 private 생성자를 활용하라 - 유틸리티성 클래스에 주로 사용한다

---
+++ src/main/java/com/example/studyeffectivejavapart1/chpater01/item04/code/DefaultUtilityClass.java
@@ -0,0 +1,9 @@
+package com.example.studyeffectivejavapart1.chpater01.item04.code;
+
+public class DefaultUtilityClass extends UtilityClass {
+
+    public static void main(String[] args) {
+        DefaultUtilityClass utilityClass = new DefaultUtilityClass();
+        utilityClass.hello();
+    }
+}
