abe54f35ed84f0292f5a405cf441277e1e1920e3
sanghun <bsh20057@gmail.com>
2023-08-11 23:48:54 +0900

    feat(item07): 다 쓴 객체 참조를 해제하라 - 자기 메모리를 직접 관리하는 클래스라면 메모리 누수에 주의해야 한다

---
+++ src/main/java/com/example/studyeffectivejavapart1/chpater01/item07/code/listener/User.java
@@ -0,0 +1,8 @@
+package com.example.studyeffectivejavapart1.chpater01.item07.code.listener;
+
+public class User {
+
+    public void receive(String message) {
+        System.out.println(message);
+    }
+}
---
+++ src/main/java/com/example/studyeffectivejavapart1/chpater01/item07/code/stack/EmptyStackException.java
@@ -0,0 +1,5 @@
+package com.example.studyeffectivejavapart1.chpater01.item07.code.stack;
+
+// Stack 커스텀 예외
+public class EmptyStackException extends IllegalStateException {
+}
